083720d2146f42a82ce36320032ad119d53ea0d6
VaIice <rlatmdgus499@gmail.com>
2024-08-27 23:24:03 +0900

    새로고침 시 redirection 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import MainPage from './pages/MainPage';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate, useLocation, useNavigate } from "react-router-dom";
 import SearchPage from './pages/SearchPage';
 import SeriesPage from './pages/SeriesPage';
 import MoviePage from './pages/MoviePage';
 import DetailPage from './pages/DetailPage';
 import Header from './components/Layout/Header';
+import { useEffect } from 'react';
+import Redirection from './Router/Redirection';
 
 function App() {
   return (
     <>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Header/>
-        <Routes>
+        <Header />
+\        <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/movie" element={<MoviePage />} />
           <Route path="/series" element={<SeriesPage />} />
           <Route path="/search/:keyword" element={<SearchPage />} />
           <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>   
       </BrowserRouter>
     </>
